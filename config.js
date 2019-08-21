837b648a5cacf1e4ca7ff465595d81690ffe5370
Cyril Okidi <okidicyril@gmail.com>
2019-08-21 17:54:13 +0300

    Add comment to config. file.

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 'use strict';
-
-//default tree map
+/**
+ * This is the default tree.
+ * Modify to your preferred setting.
+ * Only a valid JSON Object or file system path allowed.
+ */
 module.exports.tree = {};

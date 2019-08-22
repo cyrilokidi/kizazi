930c94f82f1c7afa13e4b37360381249f3c70392
Cyril Okidi <okidicyril@gmail.com>
2019-08-22 18:27:41 +0300

    Refractor unit test for tree object.

--- test/object.test.js
+++ test/object.test.js
@@ -6,7 +6,7 @@ const { object, gen, value } = require('.');
 //using json object as tree
 describe('Using Object as tree', () => {
     //set tree
-    before(() => kizazi.setTree(object))
+    before(() => kizazi.setTree(object));
     //set generation
     before(() => kizazi.generation(gen));
 
@@ -23,14 +23,14 @@ describe('Using Object as tree', () => {
         expect(test, 'Generation is not null').to.not.be.null;
         expect(test, 'Generation is not undefinded').to.not.be.undefined;
         expect(test, 'Generation is an array').to.be.an('array');
-        expect(test, 'Generation includes [generation]').to.have.all.members(gen);
+        expect(test, 'Generation is gen').to.deep.equal(gen);
     });
 
     it('Should return current original', () => {
         const test = kizazi.original;
         expect(test, 'Original is not null').to.not.be.null;
         expect(test, 'Original is not undefined').to.not.be.undefined;
-        expect(test, 'Original is generation[0]').to.be.equal(gen[0]);
+        expect(test, 'Original is gen[0]').to.be.equal(gen[0]);
         expect(test, 'Original is string').to.be.string;
     });
 
@@ -39,7 +39,7 @@ describe('Using Object as tree', () => {
         expect(test, 'Parent is not null').to.not.be.null;
         expect(test, 'Parent is not undefined').to.not.be.undefined;
         expect(test, 'Parent is an array').to.be.an('array');
-        expect(test, 'Parent is generation[0, generation.length - 1]').to.include.all.members(gen.slice(0, gen - 1));
+        expect(test, 'Parent is gen[0, gen.length - 1]').to.deep.equal(gen.slice(0, gen.length - 1));
     });
 
     it('Should return current child', () => {
@@ -47,7 +47,7 @@ describe('Using Object as tree', () => {
         expect(test, 'Child is not null').to.not.be.null;
         expect(test, 'Child is not undefined').to.not.be.undefined;
         expect(test, 'Child is string').to.be.string;
-        expect(test, 'Child is generation[generation.length - 1]').to.be.equal(gen[gen.length - 1]);
+        expect(test, 'Child is gen[gen.length - 1]').to.be.equal(gen[gen.length - 1]);
     });
 
     it('Should return value', () => {

90abe409a977d59e00055ef28a091372556c9057
Cyril Okidi <okidicyril@gmail.com>
2019-08-28 11:59:37 +0300

    Initial commit.

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 'use strict';
 /**
- * Config.: Default setting.
- */
-const { tree } = require('./config');
-
-/**
  * Class
  */
-const Lib = require('./lib');
-const Obj = require('./lib/obj');
-
-module.exports = new Lib(tree.path);
-module.exports.obj = new Obj(tree.object);
+module.exports = require('./lib');
+module.exports.obj = require('./lib/obj');

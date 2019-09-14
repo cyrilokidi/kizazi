1c1640c1c32cf6a1b7a45c303fe83f9038160be3
Cyril Okidi <okidicyril@gmail.com>
2019-09-14 17:13:26 +0300

    Update test units.

--- test/index.js
+++ test/index.js
@@ -9,7 +9,8 @@ const value = 'Module value.'; // expected value
 describe('Main class', () => {
   describe('Appending path', () => {
     it('.mod == value', () => {
-      const K = new kizazi(root);
+      const K = new kizazi();
+      K.setRoot(root);
       const TEST = require(K.app(path).mod);
       TEST.should.not.be.a('null', '[TEST] != null');
       TEST.should.not.be.a('undefined', '[TEST] != undefined');
@@ -20,7 +21,8 @@ describe('Main class', () => {
   describe('Using labels', () => {
     describe('Using .setLabel', () => {
       it('.mod == value', () => {
-        const K = new kizazi(root);
+        const K = new kizazi();
+        K.setRoot(root);
         K.setLabel('label', path);
         const TEST = require(K.label('label').mod);
         TEST.should.not.be.a('null', '[TEST] != null');
@@ -31,7 +33,8 @@ describe('Main class', () => {
 
     describe('Using .setLabelMany', () => {
       it('.mod == value', () => {
-        const K = new kizazi(root);
+        const K = new kizazi();
+        K.setRoot(root);
         K.setLabelMany({ label: path, labelB: path });
         const TEST = require(K.label('label').mod);
         TEST.should.not.be.a('null', '[TEST] != null');

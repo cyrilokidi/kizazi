98e6ad601896b97a8c34b3c64b0792a7c774e415
Cyril Okidi <okidicyril@gmail.com>
2019-09-14 19:08:23 +0300

    Update test units.

--- test/index.js
+++ test/index.js
@@ -4,48 +4,77 @@ const kizazi = require('..');
 
 const root = __dirname + '/data'; // root directory path
 const path = '/folder/folder/folder/file'; // module path
+const pathB = '/folder/folder/folder'; // module path
 const value = 'Module value.'; // expected value
 
 describe('Main class', () => {
-  describe('Appending path', () => {
-    it('.mod == value', () => {
-      const K = new kizazi();
-      K.setRoot(root);
-      const TEST = require(K.app(path).mod);
-      TEST.should.not.be.a('null', '[TEST] != null');
-      TEST.should.not.be.a('undefined', '[TEST] != undefined');
-      TEST.should.be.equal(value, '[TEST] == value');
+  describe('Returning module value', () => {
+    describe('Using labels', () => {
+      describe('Using .setLabel', () => {
+        it('.val == value', () => {
+          const K = new kizazi();
+          K.setRoot(root);
+          K.setLabel('label', path);
+          const TEST = K.label('label').val;
+          TEST.should.not.be.a('null', '[TEST] != null');
+          TEST.should.not.be.a('undefined', '[TEST] != undefined');
+          TEST.should.be.equal(value, '[TEST] == value');
+        });
+      });
+
+      describe('Using .setLabelMany', () => {
+        it('.val == value', () => {
+          const K = new kizazi();
+          K.setRoot(root);
+          K.setLabelMany({ label: path, labelB: path });
+          const TEST = K.label('label').val;
+          TEST.should.not.be.a('null', '[TEST] != null');
+          TEST.should.not.be.a('undefined', '[TEST] != undefined');
+          TEST.should.be.equal(value, '[TEST] == value');
+
+          const TESTB = K.label('labelB').val;
+          TESTB.should.not.be.a('null', '[TESTB] != null');
+          TESTB.should.not.be.a('undefined', '[TESTB] != undefined');
+          TESTB.should.be.equal(value, '[TESTB] == value');
+        });
+      });
+    });
+
+    describe('Appending path', () => {
+      describe('Appending path to empty path.', () => {
+        it('.val == value', () => {
+          const K = new kizazi();
+          K.setRoot(root);
+          const TEST = K.app(path).val;
+          TEST.should.not.be.a('null', '[TEST] != null');
+          TEST.should.not.be.a('undefined', '[TEST] != undefined');
+          TEST.should.be.equal(value, '[TEST] == value');
+        });
+      });
+
+      describe('Appending path to label path', () => {
+        it('.val == value', () => {
+          const K = new kizazi();
+          K.setRoot(root);
+          K.setLabel('label', pathB);
+          const TEST = K.label('label').app('/file').val;
+          TEST.should.not.be.a('null', '[TEST] != null');
+          TEST.should.not.be.a('undefined', '[TEST] != undefined');
+          TEST.should.be.equal(value, '[TEST] == value');
+        });
+      });
     });
   });
 
-  describe('Using labels', () => {
-    describe('Using .setLabel', () => {
-      it('.mod == value', () => {
-        const K = new kizazi();
-        K.setRoot(root);
-        K.setLabel('label', path);
-        const TEST = require(K.label('label').mod);
-        TEST.should.not.be.a('null', '[TEST] != null');
-        TEST.should.not.be.a('undefined', '[TEST] != undefined');
-        TEST.should.be.equal(value, '[TEST] == value');
-      });
-    });
-
-    describe('Using .setLabelMany', () => {
-      it('.mod == value', () => {
-        const K = new kizazi();
-        K.setRoot(root);
-        K.setLabelMany({ label: path, labelB: path });
-        const TEST = require(K.label('label').mod);
-        TEST.should.not.be.a('null', '[TEST] != null');
-        TEST.should.not.be.a('undefined', '[TEST] != undefined');
-        TEST.should.be.equal(value, '[TEST] == value');
-
-        const TESTB = require(K.label('labelB').mod);
-        TESTB.should.not.be.a('null', '[TESTB] != null');
-        TESTB.should.not.be.a('undefined', '[TESTB] != undefined');
-        TESTB.should.be.equal(value, '[TESTB] == value');
-      });
+  describe('Returning module path', () => {
+    it('.path == root + path', () => {
+      const K = new kizazi();
+      K.setRoot(root);
+      K.setLabel('label', path);
+      const TEST = K.label('label').path;
+      TEST.should.not.be.a('null', '[TEST] != null');
+      TEST.should.not.be.a('undefined', '[TEST] != undefined');
+      TEST.should.be.equal(root + path, '[TEST] == root + path');
     });
   });
 });
